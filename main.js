9001645a45ad5f706a442b75d784268d4613f46d
Genário Carneiro <53302984+genariocoliveira@users.noreply.github.com>
2022-08-22 18:46:06 -0300

    V2.0

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var distancia = parseFloat(document.querySelector('#km').value)
-var consumo = parseFloat(document.querySelector('#litro').value)
-var val = parseFloat(document.querySelector('#valor').value)
+var distancia = parseFloat(document.querySelector('#km')).value
+var consumo = parseFloat(document.querySelector('#litro')).value
+var val = parseFloat(document.querySelector('#valor')).value
 
 var litros = distancia / consumo
 var gasto = litros * val
@@ -8,21 +8,6 @@ var gasto = litros * val
 var litrosD = litros.toFixed(2)
 var gastoD = gasto.toFixed(2)
 
-function limpar(){
-    // var r = document.querySelector('#res')
-    // r.remove()
-    sessionStorage.clear();
-}
-  // function mascara(){
-  //   var distancia = document.querySelector('#km')
-  //   var d = distancia.value
-  //   if(d.length === 3 ){
-  //     d = d + "km"
-  //     distancia.value = d
-  //     return true
-  //   }
-  // }
-//}
 
 function somar() {
     var distancia = parseFloat(document.querySelector('#km').value)
@@ -35,4 +20,11 @@ function somar() {
     var litrosD = litros.toFixed(2)
     var gastoD = gasto.toFixed(2)
     var resposta = document.querySelector('#res').innerHTML = `Para sua viagem, você tera que colocar ${litrosD} litros de combustível. Com o valor de R$ ${val}, você gastara R$ ${gastoD}.`
-}
+    
+    setTimeout(() => {
+      var resposta = document.querySelector('#res').innerHTML = ''
+      var distancia = document.querySelector('#km').value =''
+      var consumo = document.querySelector('#litro').value =''
+      var val = document.querySelector('#valor').value = ''
+    }, 5000);
+  }
